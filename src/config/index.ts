01c4f2709cc7d23487661429f155f971da02bde4
Jheivy Stiven Moreno Silva <jheivymoreno@gmail.com>
2025-08-07 21:29:35 -0500

    Mejoras en implementacion de pruebas

--- src/config/index.ts
+++ src/config/index.ts
@@ -10,4 +10,4 @@ export const defineConfig = (config: BoxelsConfig) => config;
 
 export { mergeBoxelsConfig as mergeConfig };
 export type { BoxelsConfig, Plugin } from './config-schema';
-export const setupTestingBoxels = resolve(__dirname, '../dom.ts');
+export const setupTestingBoxels = resolve(__dirname, '../dom.js');
--- templates/basic/test/setup.ts
+++ templates/basic/test/setup.ts
@@ -2,5 +2,4 @@ import { expect } from 'vitest';
 import '@testing-library/jest-dom';
 import * as matchers from '@testing-library/jest-dom/matchers';
 
-// Extiende expect con los matchers de jest-dom
 expect.extend(matchers);
